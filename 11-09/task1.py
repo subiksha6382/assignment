104b0a63bc0d8d7bac3c57061060f6e889813a66
subiksha6382 <144760893+subiksha6382@users.noreply.github.com>
2023-09-11 22:29:00 -0800

    Add files via upload

---
+++ 11-09/task1.py
@@ -0,0 +1,12 @@
+Name=("subiksha arulraj")
+Address=("kammalapatti salem-636202")
+Class=("Msc computer science")
+T=80
+E=80
+M=90
+Phy=90
+Che=80
+Com=95
+Total=T+E+M+Phy+Che+Com
+Avg=Total/6
+print("I'm",Name,"My address",Address,"i'm doing",Class,"My 12th mark:",T,E,M,Phy,Che,Com,"My total mark",Total,"Avg",Avg)
